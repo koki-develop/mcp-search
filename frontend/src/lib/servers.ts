248498ef850e55d37944b811e5cd47a41c16039c
koki-develop <kou.pg.0131@gmail.com>
2025-09-11 23:41:45 +0900

    chore: Use infinite query

--- frontend/src/lib/servers.ts
+++ frontend/src/lib/servers.ts
@@ -1,20 +1,75 @@
-import { useQuery } from "@tanstack/react-query";
-import { collection, getDocs } from "firebase/firestore";
+import { useInfiniteQuery } from "@tanstack/react-query";
+import {
+	collection,
+	getDocs,
+	limit,
+	type QueryConstraint,
+	type QueryDocumentSnapshot,
+	query,
+	startAfter,
+	where,
+} from "firebase/firestore";
 import type { ServerDetail } from "./api.generated";
 import { firestore } from "./firebase";
+import { bigram } from "./util";
 
 export type Server = ServerDetail & { id: string };
 
-const _listServers = async (): Promise<Server[]> => {
-	const serversCollection = collection(firestore, "servers_v0");
-	const snapshot = await getDocs(serversCollection);
-	return snapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }) as Server);
+type ListServersParams = {
+	limit: number;
+	keyword: string;
+	cursor: QueryDocumentSnapshot | null;
 };
 
-export const useServers = () => {
-	// TODO: infinite query for pagination
-	return useQuery({
-		queryKey: ["servers"],
-		queryFn: _listServers,
+const _listServers = async (
+	params: ListServersParams,
+): Promise<{ servers: Server[]; nextCursor: QueryDocumentSnapshot | null }> => {
+	const serversCollection = collection(firestore, "servers_v0");
+
+	const words = params.keyword.split(/\s+/).filter((word) => word.length > 0);
+	const nameTokensConstraints = words.reduce((acc, word) => {
+		const tokens = bigram(word);
+		acc.push(
+			...tokens.map((token) =>
+				where(`nameTokens.${token.toLowerCase()}`, "==", true),
+			),
+		);
+		return acc;
+	}, [] as QueryConstraint[]);
+
+	const ref = query(
+		serversCollection,
+		limit(params.limit),
+		...nameTokensConstraints,
+		...(params.cursor ? [startAfter(params.cursor)] : []),
+	);
+	const snapshot = await getDocs(ref);
+
+	const servers = snapshot.docs.map(
+		(doc) => ({ id: doc.id, ...doc.data() }) as Server,
+	);
+	const nextCursor =
+		snapshot.docs.length === params.limit
+			? snapshot.docs[snapshot.docs.length - 1]
+			: null;
+	return { servers, nextCursor };
+};
+
+export type UseServersParams = {
+	keyword: string;
+};
+
+export const useServers = (params: UseServersParams) => {
+	return useInfiniteQuery({
+		queryKey: ["servers", params],
+		initialPageParam: null as QueryDocumentSnapshot | null,
+		queryFn: ({ pageParam }) =>
+			_listServers({
+				keyword: params.keyword,
+				cursor: pageParam,
+				limit: 20,
+			}),
+		select: (data) => data.pages.flatMap((page) => page.servers),
+		getNextPageParam: (lastPage) => lastPage.nextCursor,
 	});
 };

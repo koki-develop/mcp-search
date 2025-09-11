f40db8d664a57612e7cd4e46f195e8820d3ca079
koki-develop <kou.pg.0131@gmail.com>
2025-09-11 21:46:40 +0900

    feat: Implement fetchMcpServers function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,9 @@
 import { getApps, initializeApp } from "firebase-admin/app";
 import { getFirestore } from "firebase-admin/firestore";
-import { setGlobalOptions } from "firebase-functions";
 import * as logger from "firebase-functions/logger";
+import { setGlobalOptions } from "firebase-functions/v2/options";
 import { onSchedule } from "firebase-functions/v2/scheduler";
+import { getV0Servers } from "./api.generated";
 
 const apps = getApps();
 if (apps.length === 0) {
@@ -30,5 +31,35 @@ export const fetchMcpServers = onSchedule("0,30 * * * *", async () => {
   logger.info("This will be run every hour at minute 0 and 30");
 
   let cursor: string | undefined;
-  do {} while (cursor !== undefined);
+  do {
+    const response = await getV0Servers({ cursor, limit: 100 });
+    if (response.status !== 200) {
+      throw new Error(
+        `Failed to fetch servers(${response.status}): ${JSON.stringify(response.data)}`,
+      );
+    }
+
+    const batch = firestore.batch();
+    let writeCount = 0;
+    for (const server of response.data.servers) {
+      const id =
+        server._meta?.["io.modelcontextprotocol.registry/official"]?.id;
+      if (!id) {
+        logger.warn(`Server does not have id: ${JSON.stringify(server)}`);
+        continue;
+      }
+      batch.set(firestore.collection("servers_v0").doc(id), server);
+      writeCount++;
+    }
+
+    if (writeCount > 0) {
+      await batch.commit();
+      logger.info(`Wrote ${writeCount} servers to Firestore.`);
+    } else {
+      logger.info("No servers to write, exiting loop.");
+    }
+
+    await new Promise((resolve) => setTimeout(resolve, 1000)); // Sleep for 3 second to avoid rate limit
+    cursor = response.data.metadata?.next_cursor;
+  } while (cursor);
 });

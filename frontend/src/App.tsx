12a0195546803de16495e0b0a068742dd7912771
koki-develop <kou.pg.0131@gmail.com>
2025-09-11 22:42:56 +0900

    chore: Add QueryClientProvider

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Home from "./pages/Home";
+
+const queryClient = new QueryClient({
+	defaultOptions: {
+		queries: {
+			gcTime: 0,
+			refetchOnWindowFocus: false,
+		},
+	},
+});
+
 export default function App() {
-	return <div>hello world</div>;
+	return (
+		<QueryClientProvider client={queryClient}>
+			<Home />
+		</QueryClientProvider>
+	);
 }

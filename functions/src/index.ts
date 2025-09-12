6e21e6782d08aeaf3e7e2124ffd09cfc3a3cbb67
koki-develop <kou.pg.0131@gmail.com>
2025-09-12 20:25:01 +0900

    fix(functions): fix

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -65,10 +65,11 @@ export const fetchMcpServers = onSchedule("0,30 * * * *", async () => {
 				const packageRegistryTypes =
 					server.packages?.map((pkg) => pkg.registry_type).filter(Boolean) ??
 					[];
-				const remoteTransportTypes = server.remotes
-					// biome-ignore lint/suspicious/noExplicitAny: ignore
-					?.map((remote) => remote.transport_type ?? (remote as any).type)
-					.filter(Boolean);
+				const remoteTransportTypes =
+					server.remotes
+						// biome-ignore lint/suspicious/noExplicitAny: ignore
+						?.map((remote) => remote.transport_type ?? (remote as any).type)
+						.filter(Boolean) ?? [];
 
 				server._meta = {
 					...server._meta,
